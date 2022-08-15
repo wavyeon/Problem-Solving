a93ece8c14acb6daddd99fdfb71f461d87fac8ef
mcboso <jjjjun0906@naver.com>
2022-08-15 00:52:50 +0000

    22/08/15 1074

--- 1074.cpp
+++ 1074.cpp
@@ -3,56 +3,39 @@
 
 using namespace std;
 
-
-int blockSize, N, x, y, count;
-// int Zblock[32768][32768];
+int N, x, y, blockSize, index;
 
 void init() {
     cin >> N >> y >> x;
     blockSize = pow(2,N);
 }
 
-void solve(int side, int startY, int startX) {
-    // cout << startY << " " << startX << " " << count << endl;
-    if(side == 1) {
-        if(startY == y && startX == x) {
-            cout << count << endl;
+void solve() {
+    int halfLength = blockSize/2;
+    for(int i = 0; i < N; i++) {
+        if(x / halfLength == 0 && y / halfLength == 0) {
+            // cout << "1사분면" << " " << halfLength << endl;
         }
-        else {
-            count++;
+        else if(x / halfLength == 1 && y / halfLength == 0) {
+            // cout << "2사분면" << " " << halfLength << endl;
+            index += pow(halfLength, 2);
         }
-        if(startY == y && startX+1 == x) {
-            cout << count << endl;
+        else if(x / halfLength == 0 && y / halfLength == 1) {
+            // cout << "3사분면" << " " << halfLength << endl;
+            index += (2 * pow(halfLength, 2));
         }
-        else {
-            count++;
+        else if(x / halfLength == 1 && y / halfLength == 1) {
+            // cout << "4사분면" << " " << halfLength << endl;
+            index += (3 * pow(halfLength, 2));
         }
-        if(startY+1 == y && startX == x) {
-            cout << count << endl;
-        }
-        else {
-            count++;
-        }
-        if(startY+1 == y && startX+1 == x) {
-            cout << count << endl;
-        }
-        else {
-            count++;
-        }
-        // Zblock[startY][startX] = count++;
-        // Zblock[startY][startX+1] = count++;
-        // Zblock[startY+1][startX] = count++;
-        // Zblock[startY+1][startX+1] = count++;
-    }
-    else {
-        solve(side/2, startY, startX);
-        solve(side/2, startY, startX + side);
-        solve(side/2, startY + side, startX);
-        solve(side/2, startY + side, startX + side);
+        x %= halfLength;
+        y %= halfLength;
+        halfLength /= 2;
     }
 }
 
 int main() {
     init();
-    solve(blockSize/2, 0, 0);
+    solve();
+    cout << index << endl;
 }
