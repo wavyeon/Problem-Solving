2ff2ea7a12cbe6b987d79d3b1b4a5c7a53aaefe9
mcboso <jjjjun0906@naver.com>
2022-08-17 11:00:34 +0000

    22/08/17 2529

--- 2529.cpp
+++ 2529.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 #define MAX_VALUE 10
 #define Selected 1
 #define notSelected 0
@@ -9,6 +11,8 @@ char inequalitySign[MAX_VALUE];
 int numberOfSign;
 int blankNumber[MAX_VALUE];
 bool isSelected[MAX_VALUE];
+vector<string> answers;
+int answer, sizeOrder;
 
 void init() {
     scanf("%d", &numberOfSign);
@@ -18,14 +22,19 @@ void init() {
 }
 
 void selectNumber(int index) {
-    cout << index << ": " << blankNumber[index] << endl;
+    // cout << index << ": " << blankNumber[index] << endl;
     if(index == numberOfSign+1) {
         //test
         // for(int i = 0; i <= numberOfSign; i++) {
         //     cout << blankNumber[i] << " ";
         // }
         // cout << endl;
-        
+        char ans[MAX_VALUE];
+        for(int i = 0; i <= numberOfSign; i++) {
+            ans[i] = blankNumber[i] + '0';
+        }
+        string str(ans);
+        answers.push_back(str);
     }
     else {
         for(int i = 0; i < 10; i++) {
@@ -57,7 +66,19 @@ void solve() {
     }
 }
 
+void print() {
+    for(int i = 0; i <= numberOfSign; i++) {
+        cout << answers[answers.size()-1][i];
+    }
+    cout << endl;
+    for(int i = 0; i <= numberOfSign; i++) {
+        cout << answers[0][i];
+    }
+    cout << endl;
+}
+
 int main() {
     init();
     solve();
+    print();
 }
