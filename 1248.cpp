7d0dde2e9f5a81d0c35c4459dd5c5a19a1e5d7e4
mcboso <jjjjun0906@naver.com>
2022-08-18 09:43:16 +0000

    22/08/18 1248

--- 1248.cpp
+++ 1248.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 #define MAX_VALUE 11
-#define Selected 1
-#define notSelected 0
 
 using namespace std;
 
 int length;
-char signMatrix[MAX_VALUE][MAX_VALUE] = {"1", };
+char signMatrix[MAX_VALUE][MAX_VALUE];
 int answer[MAX_VALUE];
-int isSelected[MAX_VALUE];
+// int isSelected[MAX_VALUE];
+bool answerFound = false;
 
 void init() {
     scanf("%d", &length);
@@ -26,27 +25,98 @@ void init() {
     }
 }
 
+int arraySum(int i, int j) {
+    int result = 0;
+    for(int k = i; k <= j; k++) {
+        result += answer[k];
+    }
+    return result;
+}
+
+bool isAvaliableNum(int sum, char sign) {
+    if( (sum > 0 && sign == '+') || (sum == 0 && sign == '0') || (sum < 0 && sign == '-') ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void solve(index idx) {
+    if(answerFound == true) {
+        return;
+    }
+    else {
+        bool avaliable = true;
+        if(idx == length+1) {
+            for(int i = 1; i <= length; i++) {
+                printf("%d ", answer[i]);
+            }
+            answerFound = true;
+        }
+        else {
+            if(signMatrix[idx][idx] == '+') {
+                for(int i = 1; i <= 10; i++) {
+                    for(int j = idx-1; j >= 1; j--) {
+                        avaliable = isAvaliableNum( arraySum(i, j), signMatrix[i][j] ); 
+                    }
+                    if(avaliable == true) {
+                        answer[idx] = 
+                    }
+                    else if(avaliable == false) {
+                        avaliable = true;
+                        continue;
+                    }
+                }
+            }
+            else if(signMatrix[idx][idx] == '-') {
+                for(int i = -10; i <= -1; i++) {
+
+                }
+            }
+            else if(signMatrix[idx][idx] == '0') {
+                
+            }
+            
+        }
+    }
+    
+    else {
+        if(signMatrix[i][i] == '+') {
+            for(int i = 1; i <= 10; i++) {
+
+            }
+        }
+        else if(signMatrix[i][i] == '-') {
+            
+        }
+        else if(signMatrix[i][i] == '0') {
+            
+        }
+    }
+}
+
 void solve() {
-    if(signMatrix[0][0] == '+') {
+    if(signMatrix[1][1] == '+') {
         for(int i = 1; i <= 10; i++) {
-            answer[0] = i;
+            answer[1] = i;
             isSelected[i] = Selected;
-            selectNextNumber(1);
+            selectNextNumber(2);
             isSelected[i] = notSelected;
         }
     }
-    else if(signMatrix[0][0] == '-') {
+    else if(signMatrix[1][1] == '-') {
         for(int i = -10; i <= -1; i++) {
-            answer[0] = i;
+            answer[1] = i;
             isSelected[i] = Selected;
-            selectNextNumber(1);
+            selectNextNumber(2);
             isSelected[i] = notSelected;
         }
     }
-    else if(signMatrix[0][0] == '0') {
-        answer[0] = 0;
+    else if(signMatrix[1][1] == '0') {
+            answer[1] = 0;
             isSelected[0] = Selected;
-            selectNextNumber(1);
+            selectNextNumber(2);
             isSelected[0] = notSelected;
     }
     // 밑에서 부터 치고 올라오자! 맨밑에 부호보고 범위 결정 후 위로 오면서 범위 좁혀오는 방식으로
