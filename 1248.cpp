bfab1177b8d689d89b55d16671575e025066277a
mcboso <jjjjun0906@naver.com>
2022-08-19 06:49:05 +0000

    22/08/19 1248

--- 1248.cpp
+++ 1248.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <stdlib.h>
-#define MAX_VALUE 11
+#define MAX_VALUE 12
 
 using namespace std;
 
 int length;
 char signMatrix[MAX_VALUE][MAX_VALUE];
 int answer[MAX_VALUE];
-// int isSelected[MAX_VALUE];
 bool answerFound = false;
 
 void init() {
@@ -17,37 +15,46 @@ void init() {
             scanf(" %1c", &signMatrix[i][j]);
         }
     }
-    for(int i = 1; i <= length; i++) {
-        for(int j = 1; j <= length; j++) {
-            printf("%c", signMatrix[i][j]);
-        }
-        printf("\n");
-    }
 }
 
-int arraySum(int i, int j) {
-    int result = 0;
-    for(int k = i; k <= j; k++) {
-        result += answer[k];
-    }
-    return result;
+bool signCheck(int num, char sign) {
+    // if(num <= 0 && sign == '+') return false;
+    // if(num == 0 && sign != '0') return false;
+    // if(num >= 0 && sign == '-') return false;
+    // return true;
+    if(num > 0 && sign == '+') return true;
+    if(num == 0 && sign == '0') return true;
+    if(num < 0 && sign == '-') return true;
+    return false;
+    // if( (num > 0 && sign == '+') || (num == 0 && sign == '0') || (num < 0 && sign == '-') ) {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
 }
 
-bool isAvaliableNum(int sum, char sign) {
-    if( (sum > 0 && sign == '+') || (sum == 0 && sign == '0') || (sum < 0 && sign == '-') ) {
-        return true;
-    }
-    else {
+bool isAvaliableNum(int idx, int num) {
+    bool avaliable = true;
+    if(!signCheck(num, signMatrix[idx][idx])) {
         return false;
     }
+    int rangeSum = num;
+    for(int i = idx-1; i > 0; i--) {
+        if(avaliable == false) {
+            break;
+        }
+        rangeSum += answer[i];
+        avaliable = signCheck(rangeSum, signMatrix[i][idx]);
+    }
+    return avaliable;    
 }
 
-void solve(index idx) {
+void solve(int idx) {
     if(answerFound == true) {
         return;
     }
     else {
-        bool avaliable = true;
         if(idx == length+1) {
             for(int i = 1; i <= length; i++) {
                 printf("%d ", answer[i]);
@@ -55,74 +62,18 @@ void solve(index idx) {
             answerFound = true;
         }
         else {
-            if(signMatrix[idx][idx] == '+') {
-                for(int i = 1; i <= 10; i++) {
-                    for(int j = idx-1; j >= 1; j--) {
-                        avaliable = isAvaliableNum( arraySum(i, j), signMatrix[i][j] ); 
-                    }
-                    if(avaliable == true) {
-                        answer[idx] = 
-                    }
-                    else if(avaliable == false) {
-                        avaliable = true;
-                        continue;
-                    }
+            for(int i = -10; i <= 10; i++) {
+                if(isAvaliableNum(idx, i) == true) { //idx번째에 i가 들어가는 것이 적합한가?
+                    // cout << "@ " << "자릿수: " << idx << "숫자: " << i << endl;
+                    answer[idx] = i;
+                    solve(idx+1);
                 }
             }
-            else if(signMatrix[idx][idx] == '-') {
-                for(int i = -10; i <= -1; i++) {
-
-                }
-            }
-            else if(signMatrix[idx][idx] == '0') {
-                
-            }
-            
         }
     }
-    
-    else {
-        if(signMatrix[i][i] == '+') {
-            for(int i = 1; i <= 10; i++) {
-
-            }
-        }
-        else if(signMatrix[i][i] == '-') {
-            
-        }
-        else if(signMatrix[i][i] == '0') {
-            
-        }
-    }
-}
-
-void solve() {
-    if(signMatrix[1][1] == '+') {
-        for(int i = 1; i <= 10; i++) {
-            answer[1] = i;
-            isSelected[i] = Selected;
-            selectNextNumber(2);
-            isSelected[i] = notSelected;
-        }
-    }
-    else if(signMatrix[1][1] == '-') {
-        for(int i = -10; i <= -1; i++) {
-            answer[1] = i;
-            isSelected[i] = Selected;
-            selectNextNumber(2);
-            isSelected[i] = notSelected;
-        }
-    }
-    else if(signMatrix[1][1] == '0') {
-            answer[1] = 0;
-            isSelected[0] = Selected;
-            selectNextNumber(2);
-            isSelected[0] = notSelected;
-    }
-    // 밑에서 부터 치고 올라오자! 맨밑에 부호보고 범위 결정 후 위로 오면서 범위 좁혀오는 방식으로
 }
 
 int main() {
     init();
-    solve();
+    solve(1);
 }
