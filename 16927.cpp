44e6a70b2f0e5696e4fab640776a4aa63d847a8d
mcboso <jjjjun0906@naver.com>
2022-08-21 11:01:06 +0000

    22/08/21 16927

--- 16927.cpp
+++ 16927.cpp
@@ -15,7 +15,14 @@ void init() {
             scanf("%d", &originalMatrix[i][j]);
         }
     }
-    rotationTime %= (height*width);
+    // int sideLength = (width+height)*2 - 4; // 테두리 길이 
+    // rotationTime = rotationTime % sideLength;
+    // rotationTime이 0이 되버리는 경우 대비
+    // for(int i = 0; i < height; i++) {
+    //     for(int j = 0; j < width; j++) {
+    //         rotatedMatrix[i][j] = originalMatrix[i][j];
+    //     }
+    // }
 }
 
 void rotateOneLayer(int horizonStart, int horizonEnd, int verticalStart, int verticalEnd) {
@@ -31,6 +38,19 @@ void rotateOneLayer(int horizonStart, int horizonEnd, int verticalStart, int ver
     for(int i = horizonEnd; i > horizonStart; i--) {
         rotatedMatrix[verticalStart][i-1] = originalMatrix[verticalStart][i];
     }
+    //바로 다시 원래 matrix로 복사시켜서 다음 회전 준비
+    for(int i = verticalStart; i < verticalEnd; i++) {
+        originalMatrix[i+1][horizonStart] = rotatedMatrix[i+1][horizonStart];
+    }
+    for(int i = horizonStart; i < horizonEnd; i++) {
+        originalMatrix[verticalEnd][i+1] = rotatedMatrix[verticalEnd][i+1];
+    }
+    for(int i = verticalEnd; i > verticalStart; i--) {
+        originalMatrix[i-1][horizonEnd] = rotatedMatrix[i-1][horizonEnd];
+    }
+    for(int i = horizonEnd; i > horizonStart; i--) {
+        originalMatrix[verticalStart][i-1] = rotatedMatrix[verticalStart][i-1];
+    }
 }
 
 void solve() {
@@ -38,22 +58,29 @@ void solve() {
     int horizonEnd = width - 1;
     int verticalStart = 0;
     int verticalEnd = height - 1;
-    for(int j = 0; j < rotationTime; j++) {
-        for(int i = 0; i < min(height, width) / 2; i++) {
+    for(int i = 0; i < min(height, width) / 2; i++) {
+        int sideLength = (( (height - (2*i)) + (width - (2*i)) ) * 2 - 4);
+        for(int j = 0; j < rotationTime % sideLength; j++) {
             rotateOneLayer(horizonStart+i, horizonEnd-i, verticalStart+i, verticalEnd-i);
         }
-        for(int i = 0; i < height; i++) {
-            for(int j = 0; j < width; j++) {
-                originalMatrix[i][j] = rotatedMatrix[i][j];
-            }
-        }
     }
+
+    // for(int k = 0; k < rotationTime; k++) {
+    //     for(int i = 0; i < min(height, width) / 2; i++) {
+    //         rotateOneLayer(horizonStart+i, horizonEnd-i, verticalStart+i, verticalEnd-i);
+    //     }
+    //     for(int i = 0; i < height; i++) {
+    //         for(int j = 0; j < width; j++) {
+    //             originalMatrix[i][j] = rotatedMatrix[i][j];
+    //         }
+    //     }
+    // }
 }
 
 void print() {
     for(int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
-            printf("%d ", rotatedMatrix[i][j]);
+            printf("%d ", originalMatrix[i][j]);
         }
         printf("\n");
     }
