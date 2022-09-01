3a9dba0f667daa12d22da0925bb2b078dd157241
mcboso <jjjjun0906@naver.com>
2022-09-01 07:22:11 +0000

    22/09/01 13549

--- 13549.cpp
+++ 13549.cpp
@@ -12,13 +12,14 @@ using namespace std;
 int MAX = 100001;
 int from, to;
 
-// pair 안에는 구조체 사용이 불가능하다!!
+// pair 안에는 구조체 사용이 불가능하다!! -> 아닌 듯?
 typedef struct {
     int time;
     int location;
 } route;
 
-priority_queue<pair<int,pair<int, int>>> pq;
+priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+// priority_queue< pair<int,pair<int, int>>, vector<pair<int,pair<int, int>>>, greater<pair<int,pair<int, int>>> > pq;
 int isVisited[MAX_VALUE];
 
 void init() {
@@ -27,37 +28,33 @@ void init() {
 
 void bfs() {
     while(!pq.empty()) {
-        cout <<"시발" <<endl;
         route cur;
-        cur.time = pq.top().second.first;
-        cur.location = pq.top().second.second;
+        cur.time = pq.top().first;
+        cur.location = pq.top().second;
         pq.pop();
-        cout << cur.time << " " << cout.location << endl;
         if(cur.location == to) {
-            
             printf("%d\n", cur.time);
+            return;
         }
         else {
             route next;
+            if(cur.location*2 < MAX && isVisited[cur.location*2] == notVisited) { // visited 처리때문에 가장 우수한 경우인 순간이동을 가장 먼저 푸시!
+                isVisited[cur.location*2] = visited;
+                next.location = cur.location*2;
+                next.time = cur.time;
+                pq.push(make_pair(next.time, next.location));
+            }
             if(cur.location-1 >= 0 && isVisited[cur.location-1] == notVisited) {
                 isVisited[cur.location-1] = visited;
                 next.location = cur.location-1;
                 next.time = cur.time+1;
-                pq.push(make_pair(1,make_pair(next.time, next.location)));
+                pq.push(make_pair(next.time, next.location));
             }
-            if(cur.location < from) {
-                if(cur.location+1 < MAX && isVisited[cur.location+1] == notVisited) {
-                    isVisited[cur.location+1] = visited;
-                    next.location = cur.location+1;
-                    next.time = cur.time+1;
-                    pq.push(make_pair(1,make_pair(next.time, next.location)));
-                }
-                if(cur.location*2 < MAX && isVisited[cur.location*2] == notVisited) {
-                    isVisited[cur.location*2] = visited;
-                    next.location = cur.location*2;
-                    next.time = cur.time;
-                    pq.push(make_pair(0,make_pair(next.time, next.location)));
-                }
+            if(cur.location+1 < MAX && isVisited[cur.location+1] == notVisited) {
+                isVisited[cur.location+1] = visited;
+                next.location = cur.location+1;
+                next.time = cur.time+1;
+                pq.push(make_pair(next.time, next.location));
             }
         }
     }
@@ -68,7 +65,7 @@ void solve() {
     start.time = 0;
     start.location = from;
     isVisited[start.location] = visited;
-    pq.push(make_pair(0, make_pair(start.time, start.location)));
+    pq.push(make_pair(start.time, start.location));
     bfs();
 }
 
