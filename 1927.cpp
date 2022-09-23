a5e2d0ba2764fef1e8cbc40c1c44f195c20e99d5
mcboso <jjjjun0906@naver.com>
2022-09-23 06:37:09 +0000

    22/09/23 1927

---
+++ 1927.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int times, inst;
+priority_queue< int, vector<int>, greater<int> > pq;
+
+void solve() {
+    cin >> times;
+    for(int i = 0; i < times; i++) {
+        cin >> inst;
+        if(inst == 0) {
+            if(pq.empty()) {
+                cout << "0" << "\n";  
+            }
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(inst);
+        }
+    }
+}
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+    solve();
+}
