ab477f6eb147095b1716cf60d8df6fac9e6bc9ea
mcboso <jjjjun0906@naver.com>
2022-10-01 01:22:32 +0000

    22/10/01 15663

--- 15663.cpp
+++ 15663.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include <set>
+#include <map>
+#include <algorithm>
 
-using namespace;
+using namespace std;
 
-int n, m;, x
+int n, m, x;
 const int MAX = 9;
-multiset<int> arr;
-multiset<int>::iterator it;
-int ourput[MAX];
-bool isSelected[MAX];
+multimap<int, bool> arr;
+int output[MAX];
 
 void init() {
     cin.tie(0);
@@ -17,20 +16,28 @@ void init() {
     cin >> n >> m;
     for(int i = 0; i < n; i++) {
         cin >> x;
-        set.insert(x);
+        arr.insert(make_pair(x, false));
     }
 }
 
 void solve(int idx) {
-    if(idx > m) {
+    if(idx == m) {
         for(int i = 0; i < m; i++) {
             cout << output[i] << " ";
         }
         cout << "\n";
     }
     else {
+        int prev = 0;
+        multimap<int,bool>::iterator it;
         for(it = arr.begin(); it != arr.end(); it++) { // 직전의 아웃풋과 같으면 패스하는 식으로 중복 거르자
-            if()
+            if(it->second == false && prev != it->first) {
+                prev = it->first;
+                output[idx] = it->first;
+                it->second = true;
+                solve(idx+1);
+                it->second = false;
+            }
         }
     }
 }
@@ -38,7 +45,4 @@ void solve(int idx) {
 int main() {
     init();
     solve(0);
-
-
-
 }
