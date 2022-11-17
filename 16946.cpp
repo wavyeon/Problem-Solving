21c54e180ddc0ec33050d8fd35e1cc5704f4110c
Mcboso <jjjjun0906@naver.com>
2022-11-17 14:36:02 +0000

    22/11/17 16946

--- 16946.cpp
+++ 16946.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <set>
 #define MAX 1001
 #define ALL_DIR 4
 #define endl '\n'
@@ -8,11 +9,11 @@
 using namespace std;
 
 int n, m;
-vector<int> zeroIndex(1);
+vector<int> areaSize(1);
 int map[MAX][MAX];
-int zeroMap[MAX][MAX];
+int areaNum[MAX][MAX];
 int answer[MAX][MAX];
-
+bool visited[MAX][MAX];
 int dx[ALL_DIR] = {0, 1, 0, -1};
 int dy[ALL_DIR] = {-1, 0, 1, 0};
 
@@ -38,31 +39,33 @@ void zeroBfs(int i, int j, int idx) {
     queue<coo> q;
     coo start = {j, i};
     q.push(start);
-    zeroMap[i][j] = idx;
+    areaNum[i][j] = idx;
+    visited[i][j] = true;
     while(!q.empty()) {
         coo cur = q.front();
         q.pop();
-        for(int i = 0; i < ALL_DIR; i++) {
-            int nextX = cur.x + dx[i];
-            int nextY = cur.y + dy[i];
+        for(int k = 0; k < ALL_DIR; k++) {
+            int nextX = cur.x + dx[k];
+            int nextY = cur.y + dy[k];
             if(nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
-                if(map[nextY][nextX] == 0 && zeroMap[nextY][nextX] == 0) {
+                if(map[nextY][nextX] == 0 && visited[nextY][nextX] == false) {
                     coo next = {nextX, nextY};
                     q.push(next);
                     count++;
-                    zeroMap[nextY][nextX] = idx;
+                    areaNum[nextY][nextX] = idx;
+                    visited[nextY][nextX] = true;
                 }
             }
         }
     }
-    zeroIndex.push_back(count);
+    areaSize.push_back(count);
 }
 
 void classifyZero() {
     int idx = 1;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(map[i][j] == 0 && zeroMap[i][j] == 0) {
+            if(map[i][j] == 0 && visited[i][j] == false) {
                 zeroBfs(i, j, idx);
                 idx++;
             }
@@ -71,15 +74,19 @@ void classifyZero() {
 }
 
 int countMovable(int i, int j) {
-    bool idxCheck[zeroIndex.size()] = {false, };
+    set<int> visitedArea;
     int count = 1;
     for(int k = 0; k < ALL_DIR; k++) {
         int adjacentX = j + dx[k];
         int adjacentY = i + dy[k];
-        if(map[adjacentY][adjacentX] == 0 && idxCheck[zeroMap[adjacentY][adjacentX]] == false) {
-            count += zeroIndex[zeroMap[adjacentY][adjacentX]];
-            count %= 10;
-            idxCheck[zeroMap[adjacentY][adjacentX]] = true;
+        if(adjacentX >= 0 && adjacentX < m && adjacentY >= 0 && adjacentY < n) {
+            if(map[adjacentY][adjacentX] == 0) {
+                if(visitedArea.find(areaNum[adjacentY][adjacentX]) == visitedArea.end())  {// visited에서 못찾았음 == 방문한 적 없음
+                    count += areaSize[areaNum[adjacentY][adjacentX]];
+                    count %= 10;
+                    visitedArea.insert(areaNum[adjacentY][adjacentX]);
+                }
+            }
         }
     }
     return count;
@@ -91,9 +98,6 @@ void solve() {
         for(int j = 0; j < m; j++) {
             if(map[i][j] == 1) {
                 answer[i][j] = countMovable(i, j);
-            }
-            else {
-                answer[i][j] = 0;
             }
         }
     }
