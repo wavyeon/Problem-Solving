44d05224f9b551bf05bfb9450af1a89bb240c2d9
Mcboso <jjjjun0906@naver.com>
2022-11-20 14:43:25 +0000

    22/11/20 16947

--- 16947.cpp
+++ 16947.cpp
@@ -5,24 +5,91 @@
 
 using namespace std;
 
-int station, from, to, testCase;
+int station, from, to, testCase, cycleStart;
 const int MAX = 3001;
 vector<vector<int>> graph(MAX);
-bool visited[station] = {false};
+bool dfsVisited[MAX];
+bool bfsVisited[MAX];
+stack<int> st;
+vector<int> cycle;
+bool cycleIndex[MAX];
+bool cycleFound;
+
+typedef struct {
+    int num;
+    int dist;
+} subway;
 
 void init() {
     cin.tie(0);
     cout.tie(0);
     ios::sync_with_stdio(false);
     cin >> station;
-    cin >> from >> to;
-    graph[from].push_back(to);
-    graph[to].push_back(from);
+    for(int i = 0; i < station; i++) {
+        cin >> from >> to;
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+    }
+}
+
+void dfs(int prev, int cur, vector<int> history) {
+    // cout << "현재: " << cur << endl;
+    // for(int i = 0; i < history.size(); i++) {
+    //     cout << history[i] << " ";
+    // }
+    // cout << endl;
+    for(int i = 0; i < graph[cur].size(); i++) {
+        if(cycleFound == true) {
+            return;
+        }   
+        int next = graph[cur][i];
+        if(next != prev) { // 직전 방문이 아님
+            if(dfsVisited[next] == true) { // 이전에 방문한 적 있음 -> 사이클 발견
+                cycleStart = next;
+                cycle = history;
+                // cout << "!" << endl;
+                // for(int i = 0; i < history.size(); i++) {
+                //     cout << history[i] << " ";
+                // }
+                cycleFound = true;
+                // return;
+            }
+            else {
+                st.push(next);
+                dfsVisited[next] = true;
+                history.push_back(next);
+                dfs(cur, next, history);
+                history.erase(history.begin() + history.size() - 1);
+            }
+        }
+    }
 }
 
 void findCycle() {
-    for(int i = 0; i < station; i++) {
+    st.push(1);
+    dfsVisited[1] = true;
+    vector<int> v;
+    v.push_back(1);
+    dfs(0, 1, v);
+    // cout << cycleStart << endl;
+    for(int i = 0; i < cycle.size(); i++) {
+        // cout << cycle[i] << " ";
+        cycleIndex[cycle[i]] = true;
+    }
+}
+
+void bfs(int start) {
+    queue<subway> q;
+    subway depart = {start, 0};
+    q.push(depart);
+    while(!q.empty()) {
         
+    }
+}
+
+void findDist() {
+    for(int i = 1; i <= station; i++) {
+        bfs(i);
     }
 }
 
