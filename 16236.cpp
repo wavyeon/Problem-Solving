cf45d3a3dc0696c8a6b7ce4b30895b0308ed4b49
Mcboso <jjjjun0906@naver.com>
2022-11-27 14:40:20 +0000

    22/11/27 16236

--- 16236.cpp
+++ 16236.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 typedef struct {
+    int time;
     int x;
     int y;
-    int time;
 } shark;
 
 int n, fishCount, startX, startY, huntTime, eatenFish;
@@ -36,53 +36,54 @@ void init() {
     }
 }
 
-// 더 이상 먹을 물고기가 있는 지 판단
-// fishCount == 0 이거나 그래프 탐색을 마쳤는데도 먹을 물고기가 없는 경우임
-
+// priority queue 써야함! struct 대신 pair 사용하는 식으로 ㄱㄱ
 bool findFishAndMove() {
     if(fishCount == 0) {
         return false;
     }
-    queue<shark> q;
+    priority_queue<shark, vector<shark>, greater<shark>> q;
     bool visited[MAX][MAX] = {false, };
-    shark start = {startX, startY, huntTime};
+    shark start = {huntTime, startX, startY};
     visited[startY][startX] = true;
     q.push(start);
     while(!q.empty()) {
-        shark cur = q.front();
+        shark cur = q.top();
         q.pop();
+        int check = ocean[cur.y][cur.x];
+        if(check > 0 && check < 7 && check < curSize) {
+            startX = cur.x;
+            startY = cur.y;
+            huntTime = cur.time;
+            ocean[cur.y][cur.x] = 0;
+            eatenFish++;
+            if(eatenFish == curSize) {
+                curSize++;
+                eatenFish = 0;
+            }
+            fishCount--;
+            return true;
+        }
         int nextX, nextY;
         for(int i = 0; i < 4; i++) {
             nextX = cur.x + dx[i];
             nextY = cur.y + dy[i];
             if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < n) {
                 if(visited[nextY][nextX] == false) {
-                    if(ocean[nextY][nextX] > 0 && ocean[nextY][nextX] < 7 && ocean[nextY][nextX] < curSize) { // 먹을 수 있는 물고기 발견
-                        startX = nextX;
-                        startY = nextY;
-                        huntTime = cur.time+1;
-                        ocean[nextY][nextX] = 0;
-                        if(ocean[cur.y][cur.x] = 9) {
-                            ocean[cur.y][cur.x] = 0;
-                        }
-                        eatenFish++;
-                        if(eatenFish == curSize) {
-                            curSize++;
-                            eatenFish = 0;
-                        }
-                        return true;
-                    }
-                    if(ocean[nextY][nextX] == 0 || ocean[nextY][nextX] == curSize) { // 갈 수 있는 공간 발견
-                        shark next = {nextX, nextY, cur.time+1};
+                    int target = ocean[nextY][nextX];
+                    if(target == 0 || target == curSize || target == 9 || (target > 0 && target < 7 && target < curSize)) {// 빈 공간, 같은 크기의 물고기, 출발한 공간, 먹을 수 있는 물고기
+                        shark next = {cur.time+1, nextX, nextY};
                         visited[nextY][nextX] = true;
                         q.push(next);
-                        if(ocean[cur.y][cur.x] = 9) {
-                            ocean[cur.y][cur.x] = 0;
-                        }
+                        // if(cur.x == 2 && cur.y == 0) {
+                        //     cout << "!" << nextX << " " << nextY << "!" << endl;
+                        // }
+                        // if(cur.x == 3 && cur.y == 0) {
+                        //     cout << "!" << nextX << " " << nextY << "!" << endl;
+                        // }
                     }
-                    
                 }
             }
+
         }
     }
     return false;
@@ -92,15 +93,15 @@ bool findFishAndMove() {
 // 발견했을 때 바로 값 바꾸지 말고 queue에 넣은 후 cur에서 발견했을 때 값 바꾸는 식으로 ㄱㄱ
 void solve() {
     while(fishExist) {
-        cout << startX << " " << startY << " " << curSize << endl;
-        if(startX == 1 && startY == 3) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    cout << ocean[i][j] << " ";
-                }
-                cout << endl;
-            }
-        }
+        cout << startX << " " << startY << " " << huntTime << " " << curSize << endl;
+        // if(startX == 1 && startY == 3) {
+        //     for(int i = 0; i < n; i++) {
+        //         for(int j = 0; j < n; j++) {
+        //             cout << ocean[i][j] << " ";
+        //         }
+        //         cout << endl;
+        //     }
+        // }
         fishExist = findFishAndMove();
     }
     cout << huntTime << endl;
