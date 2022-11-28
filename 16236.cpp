5076b296c388a3e933c3caac9d118fc49c3556e6
Mcboso <jjjjun0906@naver.com>
2022-11-28 03:34:34 +0000

    22/11/28 16236

--- 16236.cpp
+++ 16236.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-typedef struct {
-    int time;
-    int x;
-    int y;
-} shark;
-
 int n, fishCount, startX, startY, huntTime, eatenFish;
 int curSize = 2;
 int ocean[MAX][MAX];
@@ -37,24 +31,29 @@ void init() {
 }
 
 // priority queue 써야함! struct 대신 pair 사용하는 식으로 ㄱㄱ
+// make_pair 보다 쉬운 초기화 방법?
+// 구조체 쓰면서 priority_queue를 쓰는 방법?
+// 1934번도 풀이하자잉
+// 최소 공배수를 구하는 새로운 방법 get
 bool findFishAndMove() {
     if(fishCount == 0) {
         return false;
     }
-    priority_queue<shark, vector<shark>, greater<shark>> q;
+    priority_queue< pair<int,pair<int, int>>, vector<pair<int,pair<int, int>>>, greater<pair<int,pair<int, int>>> > pq;
     bool visited[MAX][MAX] = {false, };
-    shark start = {huntTime, startX, startY};
     visited[startY][startX] = true;
-    q.push(start);
-    while(!q.empty()) {
-        shark cur = q.top();
-        q.pop();
-        int check = ocean[cur.y][cur.x];
-        if(check > 0 && check < 7 && check < curSize) {
-            startX = cur.x;
-            startY = cur.y;
-            huntTime = cur.time;
-            ocean[cur.y][cur.x] = 0;
+    pq.push(make_pair(huntTime, make_pair(startY, startX)));
+    while(!pq.empty()) {
+        int curX = pq.top().second.second;
+        int curY = pq.top().second.first;
+        int curTime = pq.top().first;
+        pq.pop();
+        int check = ocean[curY][curX];
+        if(check > 0 && check < 7 && check < curSize) { // 물고기 먹음
+            startX = curX;
+            startY = curY;
+            huntTime = curTime;
+            ocean[curY][curX] = 0;
             eatenFish++;
             if(eatenFish == curSize) {
                 curSize++;
@@ -65,21 +64,14 @@ bool findFishAndMove() {
         }
         int nextX, nextY;
         for(int i = 0; i < 4; i++) {
-            nextX = cur.x + dx[i];
-            nextY = cur.y + dy[i];
+            nextX = curX + dx[i];
+            nextY = curY + dy[i];
             if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < n) {
                 if(visited[nextY][nextX] == false) {
                     int target = ocean[nextY][nextX];
                     if(target == 0 || target == curSize || target == 9 || (target > 0 && target < 7 && target < curSize)) {// 빈 공간, 같은 크기의 물고기, 출발한 공간, 먹을 수 있는 물고기
-                        shark next = {cur.time+1, nextX, nextY};
                         visited[nextY][nextX] = true;
-                        q.push(next);
-                        // if(cur.x == 2 && cur.y == 0) {
-                        //     cout << "!" << nextX << " " << nextY << "!" << endl;
-                        // }
-                        // if(cur.x == 3 && cur.y == 0) {
-                        //     cout << "!" << nextX << " " << nextY << "!" << endl;
-                        // }
+                        pq.push(make_pair(curTime+1, make_pair(nextY, nextX)));
                     }
                 }
             }
@@ -89,56 +81,13 @@ bool findFishAndMove() {
     return false;
 }
 
-
 // 발견했을 때 바로 값 바꾸지 말고 queue에 넣은 후 cur에서 발견했을 때 값 바꾸는 식으로 ㄱㄱ
 void solve() {
     while(fishExist) {
-        cout << startX << " " << startY << " " << huntTime << " " << curSize << endl;
-        // if(startX == 1 && startY == 3) {
-        //     for(int i = 0; i < n; i++) {
-        //         for(int j = 0; j < n; j++) {
-        //             cout << ocean[i][j] << " ";
-        //         }
-        //         cout << endl;
-        //     }
-        // }
         fishExist = findFishAndMove();
     }
     cout << huntTime << endl;
 }
-
-// bool isFishExist(int x, int y) {
-    
-//     return findFish(x, y);
-// }
-
-// void moveShark(int x, int y) {
-
-// }
-
-// void solve() {
-//     while(true) {
-//         if(!isFishExist(startX, startY)) { // 물고기가 없는 경우
-//             cout << huntTime << endl;
-//             return;
-//         }
-//         else { // 물고기가 있는 경우
-//             moveShark(startX, startY);
-//         }
-//     }
-    
-
-//     if(fishCount == 0) {
-//         cout << time << endl;
-//     }
-
-//     whereToMove();
-
-//     queue<shark> move;
-    
-
-
-// }
 
 int main() {
     init();
