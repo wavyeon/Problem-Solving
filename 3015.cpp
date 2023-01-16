b68677a81c8ff7b7c4031fc1799bbf6e236bb3b7
Mcboso <jjjjun0906@naver.com>
2023-01-16 14:34:46 +0000

    23/01/16 3015

--- 3015.cpp
+++ 3015.cpp
@@ -5,19 +5,43 @@
 
 using namespace std;
 
-int n, cnt, mid, rig;
+int n;
+long long cnt;
 int people[MAX];
-stack<int> decr;
+stack<pair<int,int>> st; // first는 키, second는 키가 같은 사람의 수
 
 void init() {
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> people[i];
-    }  
+    }
 }
 
 void solve() {
-    
+    for(int i = 0; i < n; i++) {
+        int now = people[i];
+        int sameHeight = 1;
+        while(!st.empty() && st.top().first < now) { // now보다 작은 st.top은 더이상 필요 없음
+            cnt += st.top().second;
+            st.pop();
+        }
+        if(!st.empty()) {
+            if(st.top().first == now) { // now와 st.top이 키가 같으면
+                cnt += st.top().second;
+                sameHeight = st.top().second + 1;
+                if(st.size() > 1) { // st.top의 앞에 now보다 큰 사람이 존재함, 그러나 그 중에서 가장 작은 사람과만 짝지을 수 있음
+                    cnt++;
+                }
+                st.pop();
+            }
+            else { // st.top()이 now보다 크면 
+                cnt++;
+            }
+        }
+        st.push(make_pair(now, sameHeight));
+    }
+
+    cout << cnt << endl;
 
     // for(int i = 0; i < n; i++) {
     //     if(decr.empty()) {
