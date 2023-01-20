aa6e4ae44bd0b2b2b31055985f35d6eab553756b
Mcboso <jjjjun0906@naver.com>
2023-01-20 14:06:48 +0000

    23/01/20 2239

--- 2239.cpp
+++ 2239.cpp
@@ -11,6 +11,11 @@ void init() {
     for(int i = 0; i < 9; i++) {
         for(int j = 0; j < 9; j++) {
             scanf("%1d", &puzzle[i][j]);
+            if(puzzle[i][j] != 0) {
+                visited_3x3[i/3][j/3][puzzle[i][j]] = true;
+                visited_hor9[i][puzzle[i][j]] = true;
+                visited_ver9[j][puzzle[i][j]] = true;
+            }
         }
     }
 }
@@ -19,12 +24,14 @@ void checkVisit(int i, int j, int val) {
     visited_hor9[i][val] = true;
     visited_ver9[j][val] = true;
     visited_3x3[i/3][j/3][val] = true;
+    puzzle[i][j] = val;
 }
 
 void uncheckVisit(int i, int j, int val) {
     visited_hor9[i][val] = false;
     visited_ver9[j][val] = false;
     visited_3x3[i/3][j/3][val] = false;
+    puzzle[i][j] = 0;
 }
 
 bool isVisitable(int i, int j, int val) {
@@ -38,35 +45,34 @@ bool isVisitable(int i, int j, int val) {
     return false;
 }
 
+void print() {
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            cout << puzzle[i][j];
+        }
+        cout << endl;
+    }
+}
+
 void solve(int cur) {
     if(cur == 81) {
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                cout << puzzle[i][j] << " ";
-            }
-            cout << endl;
-        }
-        return;
+        print();
+        exit(0);
     }
-    for(int idx = cur; idx < 81; idx++) {
-        int i = idx / 9;
-        int j = idx % 9;
-        if(puzzle[i][j] != 0) {
-            visited_hor9[i][puzzle[i][j]] = true;
-            visited_ver9[j][puzzle[i][j]] = true;
-            visited_3x3[i/3][j/3][puzzle[i][j]] = true;
-        }
-        else {
-            for(int k = 1; k <= 9; k++) {
-                if(isVisitable(i, j, k)) {
-                    checkVisit(i, j, k);
-                    solve(cur++);
-                    uncheckVisit(i, j, k);
-                }
+    int i = cur / 9;
+    int j = cur % 9;
+    if(puzzle[i][j] == 0) {
+        for(int k = 1; k <= 9; k++) {
+            if(isVisitable(i, j, k)) {
+                checkVisit(i, j, k);
+                solve(cur+1);
+                uncheckVisit(i, j, k);
             }
         }
     }
-    
+    else {
+        solve(cur+1);
+    }
 }
 
 int main() {
