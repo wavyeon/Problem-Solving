f83b0220f70f6852f68eb2cd23fbc195f89c4baf
Mcboso <jjjjun0906@naver.com>
2023-03-11 04:58:41 +0000

    23/03/11 9252

--- 9252.cpp
+++ 9252.cpp
@@ -1,39 +1,63 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
+	#include <iostream>
+	#include <algorithm>
+	#include <string>
+	#define endl '\n'
 
-using namespace std;
+	using namespace std;
 
-int dp[1001][1001];
-int main() {
-	string a, b; cin >> a >> b;
-	for(int i=1; i<=b.size(); i++)
-		for (int j = 1; j <= a.size(); j++) {
-			if (a[j - 1] == b[i - 1])
-				dp[i][j] = dp[i - 1][j - 1] + 1;
-			else
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+	string a, b;
+	int dp[1001][1001];
+
+	void init() {
+		cin.tie(0);
+		cout.tie(0);
+		ios::sync_with_stdio(false);
+		cin >> a >> b;
+	}
+
+	void solve() {
+		for(int i = 1; i <= b.size(); i++) {
+			for (int j = 1; j <= a.size(); j++) {
+				if (a[j - 1] == b[i - 1])
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				else
+					dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
 		}
-	int k = a.size();
-	string kk;
-    
-	for (int i = b.size(); i >= 0; i--) {
-		if (dp[i][k] == 0)
-			break;
-		for (int j = k; j >= 0; j--) {
-			if (dp[i][j] == dp[i - 1][j])
+		
+		int aSize = a.size();
+		string reverseAns;
+		
+		for(int i = b.size(); i > 0; i--) {
+			if (dp[i][aSize] == 0) { // 다음 탐색하려는 dp값이 0 (탐색 더이상 의미 없음)
 				break;
-			if (dp[i][j] != dp[i][j - 1]) {
-				kk += a[j-1];
-				k = j - 1;
+			}
+			for(int j = aSize; j > 0; j--) {
+				// 왼쪽이나 위에 같은 값 있는지 먼저 판단
+				if(dp[i][j] == dp[i-1][j]) { 
+					break;
+				}
+				if(dp[i][j] == dp[i][j-1]) {
+					continue;
+				}
+				// 같은 값 없다면 1 더해진 것 (현재 같은 문자라는 뜻)
+				reverseAns += a[j-1];
+				aSize = j - 1;
 				break;
 			}
 		}
+
+		cout << dp[b.size()][a.size()] << endl;
+		if (dp[b.size()][a.size()] == 0) {
+			return;
+		}
+		for (int i = reverseAns.size() - 1; i >= 0; i--) {
+			cout << reverseAns[i];
+		}
+		cout << endl;
 	}
-	cout << dp[b.size()][a.size()] << '\n';;
-	if (dp[b.size()][a.size()] == 0)
-		return 0;
-	for (int i = kk.size() - 1; i >= 0; i--)
-		cout << kk[i];
-	cout << '\n';
-}
+
+	int main() {
+		init();
+		solve();
+	}
