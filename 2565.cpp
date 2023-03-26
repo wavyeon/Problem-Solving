361214221c58ab9a56455000c6e42dacb00bf51b
Mcboso <jjjjun0906@naver.com>
2023-03-26 13:38:59 +0000

    23/03/26 2565

--- 2565.cpp
+++ 2565.cpp
@@ -1,70 +1,57 @@
+// LIS (가장 긴 증가하는 부분 수열) 문제와 같음
 #include <iostream>
 #include <algorithm>
- 
 #define endl "\n"
-#define MAX 110
+#define MAX 101
+
 using namespace std;
  
-struct LINE
-{
-    int Left;
-    int Right;
+struct line {
+    int left;
+    int right;
 };
  
-int N;
-int DP[MAX];
-LINE Line[MAX];
+int n;
+int dp[MAX];
+line lines[MAX];
  
 int Max(int A, int B) { if (A > B) return A; return B; }
  
-void Input()
-{
-    cin >> N;
-    for (int i = 1; i <= N; i++)
-    {
-        cin >> Line[i].Left >> Line[i].Right;
+void init() {
+    cin.tie();
+    cout.tie();
+    ios::sync_with_stdio(false);
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> lines[i].left >> lines[i].right;
     }
 }
  
-bool Cmp(LINE A, LINE B)
-{
-    if (A.Left < B.Left) return true;
-    return false;
+bool cmp(line A, line B) {
+    if (A.left < B.left) { // 왼쪽 전봇대 기준으로 정렬
+        return true; // true => 순서바꾸지 않아도 됨
+    }
+    else {
+        return false; // false => 순서 바꿔야 함
+    }
 }
  
-void Solution()
-{
-    int Correct_Line = 0;
-    sort(Line + 1, Line + N + 1, Cmp);
-    for (int i = 1; i <= N; i++)
-    {
-        DP[i] = 1;
-        for (int j = 1; j < i; j++)
-        {
-            if (Line[i].Right > Line[j].Right)
-            {
-                DP[i] = Max(DP[i], DP[j] + 1);
+void solve() {
+    int goodWire = 0;
+    sort(lines + 1, lines + n + 1, cmp); // 왼쪽 전봇대 기준으로 정렬
+    for(int i = 1; i <= n; i++) {
+        dp[i] = 1;
+        for (int j = 1; j < i; j++) {
+            if(lines[i].right > lines[j].right) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
-        Correct_Line = Max(Correct_Line, DP[i]);
+        goodWire = max(goodWire, dp[i]); // 가장 긴 증가하는 부분수열의 길이
     }
-    cout << N - Correct_Line << endl;
+    cout << n - goodWire << endl;
 }
  
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
-    return 0;
+int main(void) {
+    init();
+    solve();
 }
