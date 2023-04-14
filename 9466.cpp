b27a0dbe35dfd045cfc2c3163cb482db0e0c2b86
Mcboso <jjjjun0906@naver.com>
2023-04-14 14:35:57 +0000

    23/04/14 9466

--- 9466.cpp
+++ 9466.cpp
@@ -1,56 +1,115 @@
 #include <iostream>
-#include <queue>
-#include <cstring>
+#include <vector>
+#include <stack>
+#define endl '\n'
 #define MAX 100001
+
 using namespace std;
 
-int t, n;
-int graph[MAX];
+int test, student;
+vector<int> graph;
 bool visited[MAX];
-bool done[MAX];
-int cnt;
-void hasCycle( int node) {
 
-	visited[node] = true;
-	int next = graph[node];
-
-	
-	if (!visited[next]) {
-		hasCycle( next);
-	}
-	else if (!done[next]) {//방문은 했지만 아직 사이클이 아니라면 next까지 포함해서 사이클 완성
-		//자기 자신을 포함한 팀의 멤버를 카운트
-		for (int i = next; i != node; i = graph[i]) {
-			cnt++;
-		}
-		cnt++;
-	}
-	done[node] = true;
-}
-
-
-int main() {
-
-	ios::sync_with_stdio(false);
+void init() {
 	cin.tie(0);
 	cout.tie(0);
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		for (int i = 1; i <= n; i++) {
-			cin >> graph[i];
+	ios::sync_with_stdio(false);
+	cin >> test;
+	for(int i = 0; i < test; i++) {
+		cin >> student;
+		int wannabe;
+		for(int j = 1; j <= student; j++) {
+			cin >> wannabe;
+			graph[j].push_back(wannabe);
+			graph[wannabe].push_back(j);
 		}
-		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) {
-				hasCycle(i);
-			}
-		}
-		cout << n-cnt << '\n';
-		cnt = 0;
-		memset(visited, false, sizeof(visited));
-		memset(done, false, sizeof(done));
 	}
-
-
-	return 0;
 }
+
+void solve() {
+	for(int i = 1; i <= student; i++) {
+		if(!visited[i]) { // 아직 방문을 안 한 노드
+			findCycle();
+
+			stack<int> st;
+			st.push(i);
+			int teammate = 0;
+			while(!st.empty()) {
+				int cur = st.top();
+				st.pop();
+				teammate++;
+				for(int j = 0; j < graph[cur].size(); j++) {
+					if(!visited[j]) {
+						st.push(j);
+					}
+				}
+			}				
+		}
+	}
+}
+
+int main() {
+	init();
+	solve();
+}
+
+
+
+
+
+// #include <iostream>
+// #include <queue>
+// #include <cstring>
+// #define MAX 100001
+// using namespace std;
+
+// int t, n;
+// int graph[MAX];
+// bool visited[MAX];
+// bool done[MAX];
+// int cnt;
+// void hasCycle( int node) {
+
+// 	visited[node] = true;
+// 	int next = graph[node];
+
+	
+// 	if (!visited[next]) {
+// 		hasCycle( next);
+// 	}
+// 	else if (!done[next]) {//방문은 했지만 아직 사이클이 아니라면 next까지 포함해서 사이클 완성
+// 		//자기 자신을 포함한 팀의 멤버를 카운트
+// 		for (int i = next; i != node; i = graph[i]) {
+// 			cnt++;
+// 		}
+// 		cnt++;
+// 	}
+// 	done[node] = true;
+// }
+
+
+// int main() {
+
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(0);
+// 	cout.tie(0);
+// 	cin >> t;
+// 	while (t--) {
+// 		cin >> n;
+// 		for (int i = 1; i <= n; i++) {
+// 			cin >> graph[i];
+// 		}
+// 		for (int i = 1; i <= n; i++) {
+// 			if (!visited[i]) {
+// 				hasCycle(i);
+// 			}
+// 		}
+// 		cout << n-cnt << '\n';
+// 		cnt = 0;
+// 		memset(visited, false, sizeof(visited));
+// 		memset(done, false, sizeof(done));
+// 	}
+
+
+// 	return 0;
+// }
